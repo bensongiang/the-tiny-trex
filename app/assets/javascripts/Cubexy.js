fa6e18a040c240fbf9f6a5bc772988325f140db1
Benson Giang <bensongiang@gmail.com>
2017-05-21 14:52:57 -0700

    tried adding facebook

--- app/assets/javascripts/Cubexy.js
+++ app/assets/javascripts/Cubexy.js
@@ -135,6 +135,86 @@ $.fn.extend({Cubexy: function(options) {
 				var dataURL = canvas.toDataURL('image/png');
 				$('#'+idPreview).attr('href',dataURL);
 			});
+            $('#shareFB').click(function () {
+                var data = $('#canvas')[0].toDataURL("image/png");
+                try {
+                    blob = dataURItoBlob(data);
+                } catch (e) {
+                    console.log(e);
+                }
+                FB.getLoginStatus(function (response) {
+                    console.log(response);
+                    if (response.status === "connected") {
+                        postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+                    } else if (response.status === "not_authorized") {
+                        FB.login(function (response) {
+                            postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+                        }, {scope: "publish_actions"});
+                    } else {
+                        FB.login(function (response) {
+                            postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
+                        }, {scope: "publish_actions"});
+                    }
+                });
+            });
+            function postImageToFacebook(token, filename, mimeType, imageData, message) {
+                var fd = new FormData();
+                fd.append("access_token", token);
+                fd.append("source", imageData);
+                fd.append("no_story", true);
+
+                // Upload image to facebook without story(post to feed)
+                $.ajax({
+                    url: "https://graph.facebook.com/me/photos?access_token=" + token,
+                    type: "POST",
+                    data: fd,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    success: function (data) {
+                        console.log("success: ", data);
+
+                        // Get image source url
+                        FB.api(
+                            "/" + data.id + "?fields=images",
+                            function (response) {
+                                if (response && !response.error) {
+                                    //console.log(response.images[0].source);
+
+                                    // Create facebook post using image
+                                    FB.api(
+                                        "/me/feed",
+                                        "POST",
+                                        {
+                                            "message": "",
+                                            "picture": response.images[0].source,
+                                            "link": window.location.href,
+                                            "name": 'Look at my Tiny Trex!',
+                                            "description": message,
+                                            "privacy": {
+                                                value: 'SELF'
+                                            }
+                                        },
+                                        function (response) {
+                                            if (response && !response.error) {
+                                                /* handle the result */
+                                                console.log("Posted story to facebook");
+                                                console.log(response);
+                                            }
+                                        }
+                                    );
+                                }
+                            }
+                        );
+                    },
+                    error: function (shr, status, data) {
+                        console.log("error " + data + " Status " + shr.status);
+                    },
+                    complete: function (data) {
+                        //console.log('Post to facebook Complete');
+                    }
+                });
+            }
 
 	}
 });
