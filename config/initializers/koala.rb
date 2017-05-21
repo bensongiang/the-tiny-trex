427c8c14b623fe19a72ffafe320f02bb0eae910d
Benson Giang <bensongiang@gmail.com>
2017-05-21 14:53:38 -0700

    removed secret

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,8 @@
 Koala.configure do |config|
-  config.access_token = MY_TOKEN
+  config.access_token = 262596577539662|Dkb9zKwn0d91cOUBWXe9OZf9bPc
   config.app_access_token = MY_APP_ACCESS_TOKEN
   config.app_id = 262596577539662
-  config.app_secret = 7bdbf45e2ecea4597cb94f4de7e7a565
+  config.app_secret = 
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
